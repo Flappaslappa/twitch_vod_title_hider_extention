62bd55c9f443ac9aafc053d92e09dc846beda93d
Simon <simon@Simons-MBP.fios-router.home>
2017-10-30 02:43:33 -0400

    first init

---
+++ content.js
@@ -0,0 +1,4 @@
+var text = document.getElementsByTagName('video-preview-card__title video-preview-card__title--twoline overflow-hidden relative mg-t-05 font-size-5');
+for (var i = 0, l = text.length; i < l; i++) {
+  text[i] = text[i].style.display='none';
+}
